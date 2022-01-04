1de7754227f1732e107e7177841a3fca3a89ec02
iqpizza6349 <iqpizza6349@gmail.com>
2022-01-04 16:38:35 +0900

    ✅ LoginOut

--- src/main/java/com/github/iqpizza6349/flower_tood/controller/UserController.java
+++ src/main/java/com/github/iqpizza6349/flower_tood/controller/UserController.java
@@ -3,8 +3,14 @@ package com.github.iqpizza6349.flower_tood.controller;
 import com.github.iqpizza6349.flower_tood.dto.UserDTO;
 import com.github.iqpizza6349.flower_tood.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RequiredArgsConstructor
 @Controller
@@ -16,6 +22,13 @@ public class UserController {
     public String signUp(UserDTO userDTO) {
         userService.save(userDTO);
         return "redirect:/login";
+    }
+
+    @GetMapping(value = "/logout")
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder
+                .getContext().getAuthentication());
+        return "redirect:/";
     }
 
 }
